b8ddeb877000754149a1aff30b7199545e5b2e95
MarcusJones <jones.0bj3@gmail.com>
2017-05-09 10:35:12 +0200

    First testing after revival of code

--- idf/tests/test.py
+++ idf/tests/test.py
@@ -3,14 +3,27 @@
 #===============================================================================
 # Standard:
 
-from config.config import *
+from config import *
 
 import logging.config
 import unittest
 
-from ExergyUtilities.utility_inspect import get_self
+from os import sys, path
+
+
+# Add parent to path
+if __name__ == '__main__' and __package__ is None:
+    this_path = path.dirname(path.dirname(path.abspath(__file__)))
+    sys.path.append(this_path)
+    logging.debug("ADDED TO PATH: ".format(this_path))
+
+
+
+from utility_inspect import get_self
 from utilities_xml import get_table_all_names, print_table
 from idf_parser import IDF
+#import idf_parser as idf_parser
+import utilities_xml as util_xml
 import os.path as path
 
 #===============================================================================
@@ -21,6 +34,17 @@ logging.config.fileConfig(ABSOLUTE_LOGGING_PATH)
 myLogger = logging.getLogger()
 myLogger.setLevel("DEBUG")
 
+import idf_parser as idf
+
+#===============================================================================
+# Directories and files
+#===============================================================================
+curr_dir = path.dirname(path.abspath(__file__))
+DIR_SAMPLE_IDF = path.abspath(curr_dir + "\..\.." + "\SampleIDFs")
+#print(DIR_SAMPLE_IDF)
+
+
+
 #===============================================================================
 # Unit testing
 #===============================================================================
@@ -30,19 +54,30 @@ class BasicTest(unittest.TestCase):
         #print "**** TEST {} ****".format(get_self())
         myLogger.setLevel("CRITICAL")
         print("Setup")
-        self.PATH_DESIGN_BUILDER = path.join(PATH_ENERGY_DB, r"IDF TRNSYS Conversion\Design Builder model\export1.idf")
-        self.PATH_DESIGN_TRNSYS = path.join(PATH_ENERGY_DB, r"IDF TRNSYS Conversion\TRNSYS model\simple r00.idf")
+        
+        #self.PATH_DESIGN_BUILDER = path.join(PATH_ENERGY_DB, r"IDF TRNSYS Conversion\Design Builder model\export1.idf")
+        #self.PATH_DESIGN_TRNSYS = path.join(PATH_ENERGY_DB, r"IDF TRNSYS Conversion\TRNSYS model\simple r00.idf")
         myLogger.setLevel("DEBUG")
 
     def test010_SimpleCreation(self):
         print("**** TEST {} ****".format(get_self()))
-        print(self.PATH_DESIGN_BUILDER)
-        design_builder_idf = IDF.from_IDF_file(self.PATH_DESIGN_BUILDER)
-        trnsys_idf = IDF.from_IDF_file(self.PATH_DESIGN_TRNSYS)
+        #print(self.PATH_DESIGN_BUILDER)
+        #design_builder_idf = IDF.from_IDF_file(self.PATH_DESIGN_BUILDER)
+        #trnsys_idf = IDF.from_IDF_file(self.PATH_DESIGN_TRNSYS)
+        test_file_path = path.join(DIR_SAMPLE_IDF,"5ZoneElectricBaseboard.idf")
+        logging.info("Test file:{}".format(test_file_path))
+        test_idf = IDF.from_IDF_file(test_file_path)
         
-        this_table = get_table_all_names(design_builder_idf)
-        print_table(this_table)
-        #print(design_builder_idf)
-        #print(IDF())
-        
+        print("All objects:")
+        table_names = util_xml.get_table_all_names(test_idf)
+        #print(table_names)
+        util_xml.print_table(table_names)
+        print("Number of objects:")
+        table_counts = util_xml.get_table_object_count(test_idf)
+        util_xml.print_table(table_counts)
+        print(test_idf.num_objects)
+
+        print("*** Loaded ***\n",test_idf)
+        print("*** ****** ***\n")
+
 
